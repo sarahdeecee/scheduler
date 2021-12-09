3fea7906d9db51a9c0fc50d9a30f8779a06a8f31
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-12-08 22:29:02 -0500

    add Empty.js

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,11 @@
+export default function Empty() {
+  return (
+    <main className="appointment__add">
+      <img
+        className="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+      />
+    </main>
+  );
+}
