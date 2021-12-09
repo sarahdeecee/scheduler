309cbbf1c89a7d438227c309ae3f8048fcfaa08a
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-12-08 21:57:12 -0500

    add styling for Appointments components

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import { action } from "@storybook/addon-actions/dist/preview";
+import React from "react";
+import classNames from "classnames";
+import "styles.scss";
+
+export default function Appointment() {
+  return (
+    <article className="appointment"></article>
+  );
+}
