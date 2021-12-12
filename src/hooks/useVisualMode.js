d7c233bcb22790daae431f7b1e1ff19af05e0b9b
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-12-12 17:37:32 -0500

    add back and history to useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,12 +3,21 @@ import { useState, useEffect } from "react";
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  console.log('mode: ',mode);
 
   const transition = (newMode) => {
+    setHistory(prev => [...prev, mode]);
     setMode(newMode);
   };
 
+  const back = () => {
+    if (history.length < 1 ) {
+      return;
+    }
+    const lastMode = history.pop();
+    setHistory(history);
+    setMode(lastMode);
+  };
+  
   return {mode, transition, back, history};
 }
 
