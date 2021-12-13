bb28728cdeebc3c4bd9be074cc6f90fd76d6b08d
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-12-13 16:03:38 -0500

    add bookInterview function to Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -31,7 +31,9 @@ export default function Application(props) {
   const setDay = day => setState({ ...state, day });
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const dailyInterviewers = getInterviewersForDay(state, state.day);
-  
+  const bookInterview = (id, interview) => {
+    console.log(id, interview);
+  };
   const parsedAppointments = dailyAppointments.map(appointment => {
     const interview = getInterview(state, appointment.interview);
 
@@ -41,6 +43,7 @@ export default function Application(props) {
         {...appointment}
         interview={interview}
         interviewers={dailyInterviewers}
+        bookInterview={bookInterview}
       />
     );
   });
